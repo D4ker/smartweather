4868812a6b7a1e97e30845c15719b66eaf6f63c7
D4ker <Alex-Daker@yandex.ru>
2019-06-28 02:16:55 +0300

    Added a simple parser

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Smart Weather</title>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+	<link rel="stylesheet" href="src/css/style.css" type="text/css">
+</head>
+<body>
+	<?php require_once("src/php/file.php"); ?>
+
+	<script src="src/js/main.js"></script>
+</body>
+</html>
