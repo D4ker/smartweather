314aebf669a79be214fd9729287283b5fa45dda8
D4ker <Alex-Daker@yandex.ru>
2019-07-17 17:07:00 +0300

    Added design for the page with authorization

--- src/php/pages/signInUp.php
+++ src/php/pages/signInUp.php
@@ -8,20 +8,27 @@
 	<meta charset="UTF-8">
 	<title>Вход/Регистрация</title>
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+	<link rel="stylesheet" href="../../css/signInUp.css" type="text/css">
 </head>
 <body>
 	<div class="container">
-		<form action="/src/php/pages/signInUp.php" method="POST">
-			<h2>Вход</h2>
-			<input type="text" name="login" placeholder="Введите логин">
-			<input type="password" name="password" placeholder="Введите пароль">
+		<div class="sign-in">
+			<form action="/src/php/pages/signInUp.php" method="POST">
+				<h2>Вход</h2>
+				<input type="text" name="login" placeholder="Введите логин">
+				<input type="password" name="password" placeholder="Введите пароль">
+				<button type="submit">Войти</button>
+			</form>
+		</div>
+		<div class="sign-up">
+			<form action="/src/php/pages/signInUp.php" method="POST">
+				<h2>Регистрация</h2>
+				<input type="text" name="login" placeholder="Введите логин">
+				<input type="password" name="password" placeholder="Введите пароль">
+				<input type="password" name="password-confirm" placeholder="Введите пароль ещё раз">
+				<button type="submit">Зарегистрироваться</button>
 		</form>
-		<form action="/src/php/pages/signInUp.php" method="POST">
-			<h2>Регистрация</h2>
-			<input type="text" name="login" placeholder="Введите логин">
-			<input type="password" name="password" placeholder="Введите пароль">
-			<input type="password" name="password-confirm" placeholder="Введите пароль ещё раз">
-		</form>
+		</div>
 	</div>
 </body>
 </html>
